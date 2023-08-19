6371bd5c02733ad46441e07ad3d439a7b23871d6
Mikaela Caron <mikaelacaron@gmail.com>
2023-08-19 01:52:53 -0400

    Create a TabView and move around files

---
+++ Basic-Car-Maintenance/Shared/Dashboard/DashboardView.swift
@@ -0,0 +1,18 @@
+//
+//  DashboardView.swift
+//  Basic-Car-Maintenance
+//
+//  Created by Mikaela Caron on 8/19/23.
+//
+
+import SwiftUI
+
+struct DashboardView: View {
+    var body: some View {
+        Text("Dashboard")
+    }
+}
+
+#Preview {
+    DashboardView()
+}
---
+++ Basic-Car-Maintenance/Shared/Settings/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Basic-Car-Maintenance
+//
+//  Created by Mikaela Caron on 8/19/23.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings")
+    }
+}
+
+#Preview {
+    SettingsView()
+}
