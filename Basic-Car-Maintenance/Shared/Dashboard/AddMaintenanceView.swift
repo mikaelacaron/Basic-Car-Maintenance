a7e07a9b0af9db9a2a98b569403faebe145692ca
Mikaela Caron <mikaelacaron@gmail.com>
2023-08-19 02:39:52 -0400

    Create AddMaintenanceView

---
+++ Basic-Car-Maintenance/Shared/Dashboard/AddMaintenanceView.swift
@@ -0,0 +1,20 @@
+//
+//  AddMaintenanceView.swift
+//  Basic-Car-Maintenance
+//
+//  Created by Mikaela Caron on 8/19/23.
+//
+
+import SwiftUI
+
+struct AddMaintenanceView: View {
+    var body: some View {
+        NavigationStack {
+            Text("Add")
+        }
+    }
+}
+
+#Preview {
+    AddMaintenanceView()
+}
--- Basic-Car-Maintenance/Shared/Dashboard/DashboardView.swift
+++ Basic-Car-Maintenance/Shared/Dashboard/DashboardView.swift
@@ -8,8 +8,25 @@
 import SwiftUI
 
 struct DashboardView: View {
+    
+    @State private var isShowingAddView = false
+    
     var body: some View {
-        Text("Dashboard")
+        NavigationStack {
+            Text("Dashboard")
+                .sheet(isPresented: $isShowingAddView) {
+                    AddMaintenanceView()
+                }
+                .toolbar {
+                    ToolbarItem(placement: .primaryAction) {
+                        Button {
+                            isShowingAddView.toggle()
+                        } label: {
+                            Image(systemName: "plus")
+                        }
+                    }
+                }
+        }
     }
 }
 
