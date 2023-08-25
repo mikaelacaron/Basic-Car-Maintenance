dd57ce533e803ecfcbff7db5ca5662489da27adf
Mikaela Caron <mikaelacaron@gmail.com>
2023-08-25 11:01:04 -0400

    Create AddVehicleView

--- Basic-Car-Maintenance/Shared/Settings/AddVehicleView.swift
+++ Basic-Car-Maintenance/Shared/Settings/AddVehicleView.swift
@@ -8,8 +8,42 @@
 import SwiftUI
 
 struct AddVehicleView: View {
+    
+    @State private var name = ""
+    @State private var make = ""
+    @State private var model = ""
+    
     var body: some View {
-        Text("Add vehcile view")
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Vehicle Name", text: $name, prompt: Text("Vehicle Name"))
+                } header: {
+                    Text("Name")
+                }
+                
+                Section {
+                    TextField("Vehicle Make", text: $make, prompt: Text("Vehicle Make"))
+                } header: {
+                    Text("Make")
+                }
+                
+                Section {
+                    TextField("Vehicle Model", text: $name, prompt: Text("Vehicle Model"))
+                } header: {
+                    Text("Model")
+                }
+            }
+            .toolbar {
+                ToolbarItem {
+                    Button {
+                        
+                    } label: {
+                        Text("Add")
+                    }
+                }
+            }
+        }
     }
 }
 
