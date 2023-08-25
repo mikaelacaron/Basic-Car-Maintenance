100c6f992dbb9f9af66b1d668139a9628bc522d8
Mikaela Caron <mikaelacaron@gmail.com>
2023-08-25 10:17:22 -0400

    Create AddMaintenanceView

--- Basic-Car-Maintenance/Shared/Dashboard/AddMaintenanceView.swift
+++ Basic-Car-Maintenance/Shared/Dashboard/AddMaintenanceView.swift
@@ -8,9 +8,41 @@
 import SwiftUI
 
 struct AddMaintenanceView: View {
+    
+    @State private var title = ""
+    @State private var date = Date()
+    @State private var notes = ""
+    
     var body: some View {
         NavigationStack {
-            Text("Add")
+            Form {
+                Section {
+                    TextField("Title of the event", text: $title, prompt: Text("The title of the event"))
+                } header: {
+                    Text("Title")
+                }
+                
+                DatePicker(selection: $date, displayedComponents: .date) {
+                    Text("Date")
+                }
+                
+                Section {
+                    TextField("Notes", text: $notes, prompt: Text("Additional Notes"), axis: .vertical)
+                } header: {
+                    Text("Notes")
+                }
+            }
+            .navigationTitle(Text("Add Maintenance Event"))
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem {
+                    Button {
+                        
+                    } label: {
+                        Text("Add")
+                    }
+                }
+            }
         }
     }
 }
