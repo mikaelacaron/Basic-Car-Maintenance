d6c2c317a2d82dfddf6bb938776d158815e4e0ba
Mikaela Caron <mikaelacaron@gmail.com>
2023-09-14 17:05:54 +0200

    Create AuthenticationViewModel

---
+++ Basic-Car-Maintenance/Shared/Authentication/AuthenticationViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  AuthenticationViewModel.swift
+//  Basic-Car-Maintenance
+//
+//  Created by Mikaela Caron on 9/14/23.
+//
+
+import Foundation
+
+final class AuthenticationViewModel: ObservableObject {
+    
+}
