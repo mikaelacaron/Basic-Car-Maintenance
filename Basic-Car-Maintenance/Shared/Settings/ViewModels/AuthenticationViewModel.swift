703787d9bf5b7a319e1b951859f6dccd5d00bec8
Mikaela Caron <mikaelacaron@gmail.com>
2023-09-17 11:59:02 +0200

    Create AuthenticationView and ViewModel move into Settings

---
+++ Basic-Car-Maintenance/Shared/Settings/ViewModels/AuthenticationViewModel.swift
@@ -0,0 +1,53 @@
+//
+//  AuthenticationViewModel.swift
+//  Basic-Car-Maintenance
+//
+//  Created by Mikaela Caron on 9/14/23.
+//
+
+import Foundation
+import FirebaseAuth
+
+enum AuthenticationState {
+    case unauthenticated
+    case authenticating
+    case authenticated
+}
+
+enum AuthenticationFlow {
+    case signIn
+    case signUp
+}
+
+@MainActor
+final class AuthenticationViewModel: ObservableObject {
+    
+    @Published var email = ""
+    @Published var password = ""
+    @Published var authenticationState: AuthenticationState = .unauthenticated
+    
+    @Published var user: User?
+    
+    func signInWithEmailPassword() async -> Bool {
+        
+        do {
+            let authResult = try await Auth.auth().signIn(withEmail: email, password: password)
+            user = authResult.user
+            return true
+        } catch {
+            return false
+        }
+    }
+    
+    func signUpWithEmailPassword() async -> Bool {
+        return false
+    }
+    
+    func signOut() {
+        
+    }
+    
+    func deleteAccount() async -> Bool {
+        return false
+    }
+}
---
+++ Basic-Car-Maintenance/Shared/Settings/Views/AuthenticationView.swift
@@ -0,0 +1,18 @@
+//
+//  AuthenticationView.swift
+//  Basic-Car-Maintenance
+//
+//  Created by Mikaela Caron on 9/17/23.
+//
+
+import SwiftUI
+
+struct AuthenticationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    AuthenticationView()
+}
