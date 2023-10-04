92948940c02ace8b8d9a92a9d4ce1c8bfdbadb1d
Mikaela Caron <mikaelacaron@gmail.com>
2023-10-04 01:59:05 -0400

    Create Widget Extensioin

---
+++ Basic-Car-Maintenance-Widget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  Basic-Car-Maintenance-Widget
+//
+//  Created by Mikaela Caron on 10/4/23.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource = "Configuration"
+    static var description = IntentDescription("This is an example widget.")
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
