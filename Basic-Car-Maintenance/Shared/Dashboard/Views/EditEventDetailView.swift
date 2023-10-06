4ce036fef787ede88d8863b2b23e206edb9dffac
Aaron Wilson <124102001+aaronthebald@users.noreply.github.com>
2023-10-05 23:06:22 -0400

    Added features that allow the user to update Maintenance event

---
+++ Basic-Car-Maintenance/Shared/Dashboard/Views/EditEventDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  EditEventDetailView.swift
+//  Basic-Car-Maintenance
+//
+//  Created by Aaron Wilson on 10/5/23.
+//
+
+import SwiftUI
+
+struct EditEventDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditEventDetailView()
+}
