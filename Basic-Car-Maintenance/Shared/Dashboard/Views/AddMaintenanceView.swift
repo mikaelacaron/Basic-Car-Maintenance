e828a43b0ba9087f90cf7076ebc985cdeebf660b
Hanh Q. Vu <82783173+hanhqvu@users.noreply.github.com>
2023-10-09 09:44:52 +0900

    Add Localization comments (#97)

--- Basic-Car-Maintenance/Shared/Dashboard/Views/AddMaintenanceView.swift
+++ Basic-Car-Maintenance/Shared/Dashboard/Views/AddMaintenanceView.swift
@@ -20,24 +20,37 @@ struct AddMaintenanceView: View {
         NavigationStack {
             Form {
                 Section {
-                    TextField("Title of the maintenance event",
-                              text: $title,
-                              prompt: Text("The title of the event"))
+                    TextField(text: $title,
+                              prompt: Text("The title of the event",
+                                           comment: "Maintenance event title text field label placeholder")) {
+                        Text("Title of the maintenance event",
+                             comment: "Maintenance event title text field label")
+                    }
                 } header: {
-                    Text("Title")
+                    Text("Title",
+                         comment: "Maintenance event title text field header")
                 }
                 
                 DatePicker(selection: $date, displayedComponents: .date) {
-                    Text("Date")
+                    Text("Date",
+                         comment: "Date picker label")
                 }
                 
                 Section {
-                    TextField("Notes", text: $notes, prompt: Text("Additional Notes"), axis: .vertical)
+                    TextField(text: $notes,
+                              prompt: Text("Additional Notes",
+                                           comment: "Notes text field placeholder"),
+                              axis: .vertical) {
+                        Text("Notes",
+                             comment: "Maintenance event notes text field label")
+                    }
                 } header: {
-                    Text("Notes")
+                    Text("Notes",
+                         comment: "Notes text field header")
                 }
             }
-            .navigationTitle(Text("Add Maintenance"))
+            .navigationTitle(Text("Add Maintenance",
+                                  comment: "Nagivation title for Add Maintenance view"))
             .toolbar {
                 ToolbarItem {
                     Button {
@@ -45,7 +58,8 @@ struct AddMaintenanceView: View {
                         addTapped(event)
                         dismiss()
                     } label: {
-                        Text("Add")
+                        Text("Add",
+                             comment: "Label for button to add data")
                     }
                     .disabled(title.isEmpty)
                 }
