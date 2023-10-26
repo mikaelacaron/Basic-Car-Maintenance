48a7fc3483fc599368f9bc23bd547d340384c49b
Horacio Mota <horaciomota93@gmail.com>
2023-10-26 16:26:06 +0100

    Enum created, listin all icons in the project

--- Basic-Car-Maintenance/Shared/Utilities/Constants.swift
+++ Basic-Car-Maintenance/Shared/Utilities/Constants.swift
@@ -16,3 +16,35 @@ enum FirestoreCollection {
 enum FirestoreField {
     static let userID = "userID"
 }
+
+enum SFSymbol {
+    //Tabview icons
+    static let dashboard = "list.dash.header.rectangle"
+    static let odometer = "gauge"
+    static let settings = "gear"
+
+    //Navigation header icons
+    static let filter = "line.3.horizontal.decrease.circle"
+    static let add = "plus"
+    
+    //Dashboard icons
+    static let trash = "trash"
+    static let edit = "pencil"
+    static let magnifyingglass = "magnifyingglass"
+    
+    //Settings icons
+    static let document = "doc.badge.plus"
+    static let reportbug = "ladybug"
+    static let contributors = "person.3.fill"
+    static let profile = "person"
+    static let appIcon = "apps.iphone"
+    
+    //ChoseAopIcon View icons
+    static let checkmark = "checkmarkImage"
+    
+    //ContributorsProfileView icon
+    static let personcircle = "person.circle.fill"
+
+    
+    
+}
