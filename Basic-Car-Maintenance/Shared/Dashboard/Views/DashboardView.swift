857f758ceb7c4a0eb635d266c10ef5f3a911658e
Jessie Linden <78987924+jessielinden@users.noreply.github.com>
2023-10-30 22:27:54 -0400

    IMPROVE - Analytics Screen View Modifier (#253)

--- Basic-Car-Maintenance/Shared/Dashboard/Views/DashboardView.swift
+++ Basic-Car-Maintenance/Shared/Dashboard/Views/DashboardView.swift
@@ -5,7 +5,6 @@
 //  Created by Mikaela Caron on 8/19/23.
 //
 
-import FirebaseAnalyticsSwift
 import SwiftUI
 
 struct DashboardView: View {
@@ -63,7 +62,7 @@ struct DashboardView: View {
                 }
                 .listStyle(.inset)
             }
-            .analyticsScreen(name: "\(Self.self)")
+            .analyticsView()
             .searchable(text: $viewModel.searchText)
             .overlay {
                 if viewModel.isLoading {
