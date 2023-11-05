32e46cee05f37e5f4016e6041974d4ef94e604a6
Mikaela Caron <mikaelacaron@gmail.com>
2023-11-05 00:11:00 -0400

    Add OnboardingView

---
+++ Basic-Car-Maintenance/Shared/MainView/Views/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  Basic-Car-Maintenance
+//
+//  Created by Mikaela Caron on 11/5/23.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
