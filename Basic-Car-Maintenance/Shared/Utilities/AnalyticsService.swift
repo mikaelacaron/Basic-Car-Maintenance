9d52f8057434861f0db68724c81e7a02a0beb7f8
Mikaela Caron <mikaelacaron@gmail.com>
2023-11-11 23:05:21 -0500

    Create AnalyticsService

---
+++ Basic-Car-Maintenance/Shared/Utilities/AnalyticsService.swift
@@ -0,0 +1,14 @@
+//
+//  AnalyticsService.swift
+//  Basic-Car-Maintenance
+//
+//  Created by Mikaela Caron on 11/11/23.
+//
+
+import Foundation
+
+class AnalyticsService {
+    private init() { }
+    
+    static let shared = AnalyticsService()
+}
