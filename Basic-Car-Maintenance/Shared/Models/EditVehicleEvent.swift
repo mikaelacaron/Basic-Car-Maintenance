89421788fa0179df4a929fc395493311bb04de81
Traton Gossink <79613749+TratonGossink@users.noreply.github.com>
2023-11-20 15:07:45 -0600

    - add EditVehicleView - add EditVehicleEvent

---
+++ Basic-Car-Maintenance/Shared/Models/EditVehicleEvent.swift
@@ -0,0 +1,8 @@
+//
+//  EditVehicleEvent.swift
+//  Basic-Car-Maintenance
+//
+//  Created by Traton Gossink on 11/18/23.
+//
+
+import Foundation
---
+++ Basic-Car-Maintenance/Shared/Settings/Views/EditVehicleView.swift
@@ -0,0 +1,8 @@
+//
+//  EditVehicleView.swift
+//  Basic-Car-Maintenance
+//
+//  Created by Traton Gossink on 11/17/23.
+//
+
+import Foundation
