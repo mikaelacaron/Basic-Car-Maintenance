345e58c1442ab7660c0b9dd4fa8faec732f8de16
Mikaela Caron <mikaelacaron@gmail.com>
2024-01-14 23:18:17 -0500

    Add PaywallView

---
+++ Basic-Car-Maintenance/Shared/MainView/Views/PaywallView.swift
@@ -0,0 +1,19 @@
+//
+//  PaywallView.swift
+//  Basic-Car-Maintenance
+//
+//  Created by Mikaela Caron on 1/14/24.
+//
+
+import RevenueCatUI
+import SwiftUI
+
+struct PaywallView: View {
+    var body: some View {
+        RevenueCatUI.PaywallView()
+    }
+}
+
+#Preview {
+    PaywallView()
+}
