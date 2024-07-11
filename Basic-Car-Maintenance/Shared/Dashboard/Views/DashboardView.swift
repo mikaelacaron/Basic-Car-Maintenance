1a6de1007ef32900d8d6d2251bef013815c9e23f
Jake Maidment <jmaidmentdev@gmail.com>
2024-07-11 21:34:13 +0100

    Task 286 - Improve DashboardView completed.

--- Basic-Car-Maintenance/Shared/Dashboard/Views/DashboardView.swift
+++ Basic-Car-Maintenance/Shared/Dashboard/Views/DashboardView.swift
@@ -13,6 +13,7 @@ struct DashboardView: View {
     @State private var isShowingAddView = false
     @State private var viewModel: DashboardViewModel
     @State private var isShowingEditView = false
+    @State private var isShowingAddVehicleView = false
     @State private var selectedMaintenanceEvent: MaintenanceEvent?
     
     init(userUID: String?) {
@@ -80,8 +81,15 @@ struct DashboardView: View {
                     ProgressView("Loading...")
                 } else {
                     if viewModel.events.isEmpty {
-                        Text("Add your first maintenance",
-                             comment: "Placeholder text for empty maintenance list prompting the user to add a maintenance event") // swiftlint:disable:this line_length
+                // Blank Dashboard view starts
+                        VStack(spacing:20){
+
+                            ContentUnavailableView("Tap the + to begin",
+                            systemImage: "wrench",
+                            description: Text("Add your first maintenance"))
+                        }
+                // Blank Dashboard view ends
+                        
                     } else if viewModel.searchedEvents.isEmpty && !viewModel.searchText.isEmpty {
                         ContentUnavailableView("No results",
                                                systemImage: SFSymbol.magnifyingGlass,
